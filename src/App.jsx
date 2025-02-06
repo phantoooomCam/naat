05cbd79b5875ae7a7396399bb6de6a4049203696
Diego Camarillo <bigocam123@gmail.com>
2025-02-06 12:32:48 -0600

    todo para corregir

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./paginas/Home";
+import Login from "./paginas/Login"
+import Registro from './paginas/Registro';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Registro" element={<Registro />} />
     </Routes>
   );
 }
