bf618e08ea9b310622d2fbd4ce11b77d422851a5
Diego Camarillo <bigocam123@gmail.com>
2025-02-06 13:56:00 -0600

    todo vacio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./paginas/Home";
-import Login from "./paginas/Login"
-import Registro from './paginas/Registro';
+import Home from "./paginas/Principal/Home";
+import Registro from "./paginas/Registrarse/Registro"
+import Login from './paginas/Signin/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Login" element={<Login />} />
       <Route path="/Registro" element={<Registro />} />
+      <Route path="/Login" element={<Login />} />
     </Routes>
   );
 }
