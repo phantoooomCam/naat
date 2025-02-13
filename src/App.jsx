d55a51dba0d330d83ba38714f472ad2bb7d5f894
SajitVentura <elopezv1700@alumno.ipn.mx>
2025-02-13 10:14:42 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./paginas/Login-SignIn/Registro"
 import SignIn from './paginas/Login-SignIn/SignIn';
 import PageNotFound from './paginas/Error/PageNotFound';
-import Auth from './paginas/Auth/Auth';
 import HomeAlt from "./paginas/HomeAlt/HomeAlt";
 
 function App() {
