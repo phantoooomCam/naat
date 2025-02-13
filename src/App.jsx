1030ae9ed3fa9f546c61190bd2f191a24048ef3c
SajitVentura <elopezv1700@alumno.ipn.mx>
2025-02-13 10:12:17 -0600

    Login-SignIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./paginas/Registrarse/Registro"
-import SignIn from './paginas/SignIn/SignIn';
+import Login from "./paginas/Login-SignIn/Registro"
+import SignIn from './paginas/Login-SignIn/SignIn';
 import PageNotFound from './paginas/Error/PageNotFound';
 import Auth from './paginas/Auth/Auth';
 import HomeAlt from "./paginas/HomeAlt/HomeAlt";
