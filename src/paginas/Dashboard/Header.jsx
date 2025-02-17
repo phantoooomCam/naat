c4d0846928342275892509f8dec2c925273b29ee
Diego Camarillo <bigocam123@gmail.com>
2025-02-17 12:22:45 -0600

    boton cerrar sesion

--- src/paginas/Dashboard/Header.jsx
+++ src/paginas/Dashboard/Header.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { FiUser, FiLogOut } from 'react-icons/fi';
 import PropTypes from 'prop-types';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const usuario = JSON.parse(localStorage.getItem("user"));
@@ -9,12 +10,23 @@ const Header = () => {
   const apellido =usuario?.apellidoPaterno || "Apellido";
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const profileRef = useRef(null);
+  
 
   const user = {
     name: 'Juan Pérez',
     email: 'juan@empresa.com',
     role: 'Administrador',
     avatar: 'https://i.pravatar.cc/100'
+  };
+
+
+
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/signin"); // Redirige a la página de inicio de sesión
   };
 
   // Cerrar el menú si el usuario hace clic fuera del perfil
@@ -60,7 +72,7 @@ const Header = () => {
               <p className="role">{user.role}</p>
             </div>
             
-            <button className="logout-btn">
+            <button className="logout-btn" onClick={handleLogout}>
               <FiLogOut className="icon" />
               Cerrar sesión
             </button>
