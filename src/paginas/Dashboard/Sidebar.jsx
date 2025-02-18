648fc65eac39e8fa9bd79c063a75c92dfbda8514
Diego Camarillo <bigocam123@gmail.com>
2025-02-18 11:12:48 -0600

    sidebar

--- src/paginas/Dashboard/Sidebar.jsx
+++ src/paginas/Dashboard/Sidebar.jsx
@@ -1,22 +1,28 @@
-import { FiHome, FiBarChart2, FiSettings, FiHelpCircle, FiUsers   } from 'react-icons/fi';
+import { FiHome, FiBarChart2, FiSettings, FiHelpCircle, FiUsers } from 'react-icons/fi';
 import { BiLogIn } from "react-icons/bi";
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';  // Importar useNavigate
 import './Sidebar.css';
 import NAAT from '../../assets/completo_blanco.png';
 
 const Sidebar = ({ activeView, setActiveView, isOpen, toggleSidebar }) => {
+  const navigate = useNavigate(); // Inicializar useNavigate
+
   const menuItems = [
-    { id: 'inicio', icon: <FiHome />, label: 'Inicio' },
-    { id: 'gestionar-usuarios', icon: <FiUsers  />, label: 'Gestionar usuarios' },
-    { id: 'Ingresos', icon: <BiLogIn />, label: 'Ingresos al sistema' },
-    { id: 'actividad-sistema', icon: <FiBarChart2 />, label: 'Actividad del sistema' },
+    { id: 'inicio', icon: <FiHome />, label: 'Inicio', path: '/dashboard' },
+    { id: 'gestionar-usuarios', icon: <FiUsers />, label: 'Gestionar usuarios', path: '/gestion' },
+    { id: 'Ingresos', icon: <BiLogIn />, label: 'Ingresos al sistema', path: '/ingresos' },
+    { id: 'actividad-sistema', icon: <FiBarChart2 />, label: 'Actividad del sistema', path: '/actividad' },
   ];
-  
+
+  const handleNavigation = (path) => {
+    navigate(path); // Redirigir a la ruta proporcionada
+  };
 
   return (
     <nav className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
       <div className="sidebar-header">
-        <div 
+        <div
           className="logo-wrapper"
           onClick={toggleSidebar}
           style={{ cursor: 'pointer' }}
@@ -35,7 +41,10 @@ const Sidebar = ({ activeView, setActiveView, isOpen, toggleSidebar }) => {
           <button
             key={item.id}
             className={`menu-item ${activeView === item.id ? 'active' : ''}`}
-            onClick={() => setActiveView(item.id)}
+            onClick={() => {
+              setActiveView(item.id);
+              handleNavigation(item.path); // Llamar a la función de navegación
+            }}
           >
             <span className="icon">{item.icon}</span>
             {isOpen && <span className="label">{item.label}</span>}
