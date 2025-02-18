236569816778b93e32eed58df1236d25d75860ea
SajitVentura <elopezv1700@alumno.ipn.mx>
2025-02-18 12:12:09 -0600

    Ingresos

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import PageNotFound from './paginas/Error/PageNotFound';
 import HomeAlt from "./paginas/HomeAlt/HomeAlt";
 import Dashboard from './paginas/Admin/Inicio/dashboard';
 import Gestion from './paginas/Admin/Gestion/Gestion';
-import IngresoSist from './paginas/Admin/Ingresos/IngresosSist';
+import IngresoSist from './paginas/Admin/Ingresos/Ingresos';
 
-import ProtectedRoute from "./componentes/ProtectedRoute"; 
+import ProtectedRoute from "./componentes/ProtectedRoute";
 
 function App() {
   return (
@@ -16,12 +16,12 @@ function App() {
       <Route path="/" element={<HomeAlt />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="*" element={<PageNotFound />}/>
+      <Route path="*" element={<PageNotFound />} />
       <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/gestion" element={<Gestion />} />
-          <Route path="/ingresos" element={<IngresoSist />} />
-        </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/gestion" element={<Gestion />} />
+        <Route path="/ingresos" element={<IngresoSist />} />
+      </Route>
     </Routes>
   );
 }
