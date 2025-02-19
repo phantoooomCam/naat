10b9b0101aa4476b13d003de82caafa6214fcca6
Diego Camarillo <bigocam123@gmail.com>
2025-02-19 09:47:28 -0600

    canbios en el avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./paginas/Login-SignIn/Registro"
-import SignIn from './paginas/Login-SignIn/SignIn';
-import PageNotFound from './paginas/Error/PageNotFound';
+import Login from "./paginas/Login-SignIn/Registro";
+import SignIn from "./paginas/Login-SignIn/SignIn";
+import PageNotFound from "./paginas/Error/PageNotFound";
 import HomeAlt from "./paginas/HomeAlt/HomeAlt";
-import Dashboard from './paginas/Admin/Inicio/dashboard';
-import Gestion from './paginas/Admin/Gestion/Gestion';
-import IngresoSist from './paginas/Admin/Ingresos/Ingresos';
-import Actividad from './paginas/Admin/Actividad/Actividad';
+import Dashboard from "./paginas/Admin/Inicio/dashboard";
+import Gestion from "./paginas/Admin/Gestion/Gestion";
+import IngresoSist from "./paginas/Admin/Ingresos/Ingresos";
+import Actividad from "./paginas/Admin/Actividad/Actividad";
 
 import ProtectedRoute from "./componentes/ProtectedRoute";
 
@@ -18,6 +18,11 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="*" element={<PageNotFound />} />
+      {/* <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/gestion" element={<Gestion />} />
+      <Route path="/ingresos" element={<IngresoSist />} />
+      <Route path="/actividad" element={<Actividad />} /> */}
+
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/gestion" element={<Gestion />} />
