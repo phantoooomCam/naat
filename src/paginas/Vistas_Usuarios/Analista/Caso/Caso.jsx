a46bc799c742737838a5d5690740f3bd6e695310
Diego Camarillo <bigocam123@gmail.com>
2025-05-02 14:11:55 -0600

    Cambios en casos

--- src/paginas/Vistas_Usuarios/Analista/Caso/Caso.jsx
+++ src/paginas/Vistas_Usuarios/Analista/Caso/Caso.jsx
@@ -1,28 +1,27 @@
-import React from "react";
-import {useState} from "react";
-import { Link, useNavigate } from "react-router-dom";
+"use client"
+import { useState } from "react"
 
-import Header from "../../../../componentes/Header";
-import Sidebar from "../../../../componentes/Sidebar";
-import "../../../../paginas/SuperAdmin_Funciones/Usuarios/Gestion/Gestion.css";
+import Header from "../../../../componentes/Header"
+import Sidebar from "../../../../componentes/Sidebar"
+import "./Caso.css"
 
 function Caso() {
-    const [activeView, setActiveView] = useState("inicio");
-    const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [activeView, setActiveView] = useState("procesamiento")
+  const [sidebarOpen, setSidebarOpen] = useState(true)
 
-    return (
-      <div className="dashboard-container">
-        <Sidebar
-          activeView={activeView}
-          setActiveView={setActiveView}
-          isOpen={sidebarOpen}
-          toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
-        />
-        <main className={`main-content ${sidebarOpen ? "" : "collapsed"}`}>
-          <Header />
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className="dashboard-container">
+      <Sidebar
+        activeView={activeView}
+        setActiveView={setActiveView}
+        isOpen={sidebarOpen}
+        toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
+      />
+      <main className={`main-content ${sidebarOpen ? "" : "collapsed"}`}>
+        <Header />
+      </main>
+    </div>
+  )
+}
 
-  export default Caso;
+export default Caso
