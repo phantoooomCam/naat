ccec031b710ccc8e1e116d7184938dd5719f694e
Diego Camarillo <bigocam123@gmail.com>
2025-05-26 13:03:09 -0600

    Update sidebarConfig.jsx

--- src/componentes/sidebarConfig.jsx
+++ src/componentes/sidebarConfig.jsx
@@ -1,133 +1,131 @@
-import { FiHome, FiUsers, FiSettings, FiHelpCircle } from 'react-icons/fi';
+import { FiHome, FiUsers, FiSettings, FiHelpCircle } from "react-icons/fi";
 import { LuBookHeadphones } from "react-icons/lu";
 import { SlOrganization } from "react-icons/sl";
 import { VscOrganization } from "react-icons/vsc";
 import { PiFolderUserDuotone } from "react-icons/pi";
 import fetchWithAuth from "../utils/fetchWithAuth";
 
-
-export const menu={
-    1: [
-        { id: '/dashboard', icon: <FiHome />, label: 'Inicio' },
-        {
-          id: 'usuarios',
-          icon: <FiUsers />,
-          label: 'Usuarios',
-          subItems: [
-            { id: '/gestion', label: 'Gestionar Usuarios' },
-            { id: '/solicitudes', label: 'Solicitudes' }
-          ]
-        },
-        {
-          id: 'sistema',
-          icon: <FiSettings />,
-          label: 'Sistema',
-          subItems: [
-            { id: '/actividad', label: 'Actividad' },
-            { id: '/ingresos', label: 'Ingresos' }
-          ]
-        },
-        {
-          id: 'organizaciones',
-          icon: <FiHelpCircle />,
-          label: 'Organizaciones',
-          subItems: [
-            { id: '/orga', label: 'Gestion Organización' },
-            { id: '/area', label: 'Gestion Area' },
-            { id: '/depto', label: 'Gestion Departamento' }
-    
-          ]
-        },
-        {
-          id: 'casos',
-          icon: <PiFolderUserDuotone />,
-          label: 'Casos',
-          subItems: [
-            { id: '/casos', label: 'Gestion Casos' },
-            { id: '/logscasos', label: 'Actividad Casos' }
-          ]
-        },
+export const menu = {
+  1: [
+    { id: "/dashboard", icon: <FiHome />, label: "Inicio" },
+    {
+      id: "usuarios",
+      icon: <FiUsers />,
+      label: "Usuarios",
+      subItems: [
+        { id: "/gestion", label: "Gestionar Usuarios" },
+        { id: "/solicitudes", label: "Solicitudes" },
       ],
-      2:[
-        { id: '/home_org', icon: <FiHome />, label: 'Inicio' },
-        { id: '/gestion', icon: <FiUsers />, label: 'Gestion Usuarios' },
-        {
-          id: 'sistema',
-          icon: <FiSettings />,
-          label: 'Sistema',
-          subItems: [
-            { id: '/actividad', label: 'Actividad' },
-            { id: '/ingresos', label: 'Ingresos' }
-          ]
-        },
-        { id: '/area', icon: <SlOrganization />, label: 'Areas' },
-        { id: '/depto', icon:<VscOrganization />, label: 'Departamenos' },
-        {
-          id: 'casos',
-          icon: <PiFolderUserDuotone />,
-          label: 'Casos',
-          subItems: [
-            { id: '/casos', label: 'Gestion Casos' },
-            { id: '/logscasos', label: 'Actividad Casos' }
-          ]
-        },
+    },
+    {
+      id: "sistema",
+      icon: <FiSettings />,
+      label: "Sistema",
+      subItems: [
+        { id: "/actividad", label: "Actividad" },
+        { id: "/ingresos", label: "Ingresos" },
       ],
-      3:[
-        { id: '/home_area', icon: <FiHome />, label: 'Inicio' },
-        { id: '/gestion', icon: <FiUsers />, label: 'Gestion Usuarios' },
-        {
-          id: 'sistema',
-          icon: <FiSettings />,
-          label: 'Sistema',
-          subItems: [
-            { id: '/actividad', label: 'Actividad' },
-            { id: '/ingresos', label: 'Ingresos' }
-          ]
-        },
-        { id: '/depto', icon:<VscOrganization />, label: 'Departamenos' },
-        {
-          id: 'casos',
-          icon: <PiFolderUserDuotone />,
-          label: 'Casos',
-          subItems: [
-            { id: '/casos', label: 'Gestion Casos' },
-            { id: '/logscasos', label: 'Actividad Casos' }
-          ]
-        },
+    },
+    {
+      id: "organizaciones",
+      icon: <FiHelpCircle />,
+      label: "Organizaciones",
+      subItems: [
+        { id: "/orga", label: "Gestion Organización" },
+        { id: "/area", label: "Gestion Area" },
+        { id: "/depto", label: "Gestion Departamento" },
       ],
-      4:[
-        { id: '/home_depto', icon: <FiHome />, label: 'Inicio' },
-        { id: '/gestion', icon: <FiUsers />, label: 'Gestion Usuarios' },
-        {
-          id: 'sistema',
-          icon: <FiSettings />,
-          label: 'Sistema',
-          subItems: [
-            { id: '/ingresos', label: 'Ingresos' }
-          ]
-        },
-        {
-          id: 'casos',
-          icon: <PiFolderUserDuotone />,
-          label: 'Casos',
-          subItems: [
-            { id: '/casos', label: 'Gestion Casos' },
-            { id: '/logscasos', label: 'Actividad Casos' }
-          ]
-        },
+    },
+    {
+      id: "casos",
+      icon: <PiFolderUserDuotone />,
+      label: "Casos",
+      subItems: [
+        { id: "/casos", label: "Gestion Casos" },
+        { id: "/logscasos", label: "Actividad Casos" },
       ],
-      5:[
-        { id: '/home_analista', icon: <FiHome />, label: 'Inicio' },
-        { id: '/casos', icon: <PiFolderUserDuotone />, label: 'Casos' },
-        {
-          id: 'casos',
-          icon: <PiFolderUserDuotone />,
-          label: 'Casos',
-          subItems: [
-            { id: '/casos', label: 'Gestion Casos' },
-            { id: '/sabana', icon: <LuBookHeadphones />, label: 'Sabanas' },
-          ]
-        },
-      ]
-
-}
+    },
+    { id: "/sabana", icon: <LuBookHeadphones />, label: "Sabanas" },
+  ],
+  2: [
+    { id: "/home_org", icon: <FiHome />, label: "Inicio" },
+    { id: "/gestion", icon: <FiUsers />, label: "Gestion Usuarios" },
+    {
+      id: "sistema",
+      icon: <FiSettings />,
+      label: "Sistema",
+      subItems: [
+        { id: "/actividad", label: "Actividad" },
+        { id: "/ingresos", label: "Ingresos" },
+      ],
+    },
+    { id: "/area", icon: <SlOrganization />, label: "Areas" },
+    { id: "/depto", icon: <VscOrganization />, label: "Departamenos" },
+    {
+      id: "casos",
+      icon: <PiFolderUserDuotone />,
+      label: "Casos",
+      subItems: [
+        { id: "/casos", label: "Gestion Casos" },
+        { id: "/logscasos", label: "Actividad Casos" },
+      ],
+    },
+    { id: "/sabana", icon: <LuBookHeadphones />, label: "Sabanas" },
+  ],
+  3: [
+    { id: "/home_area", icon: <FiHome />, label: "Inicio" },
+    { id: "/gestion", icon: <FiUsers />, label: "Gestion Usuarios" },
+    {
+      id: "sistema",
+      icon: <FiSettings />,
+      label: "Sistema",
+      subItems: [
+        { id: "/actividad", label: "Actividad" },
+        { id: "/ingresos", label: "Ingresos" },
+      ],
+    },
+    { id: "/depto", icon: <VscOrganization />, label: "Departamenos" },
+    {
+      id: "casos",
+      icon: <PiFolderUserDuotone />,
+      label: "Casos",
+      subItems: [
+        { id: "/casos", label: "Gestion Casos" },
+        { id: "/logscasos", label: "Actividad Casos" },
+      ],
+    },
+    { id: "/sabana", icon: <LuBookHeadphones />, label: "Sabanas" },
+  ],
+  4: [
+    { id: "/home_depto", icon: <FiHome />, label: "Inicio" },
+    { id: "/gestion", icon: <FiUsers />, label: "Gestion Usuarios" },
+    {
+      id: "sistema",
+      icon: <FiSettings />,
+      label: "Sistema",
+      subItems: [{ id: "/ingresos", label: "Ingresos" }],
+    },
+    {
+      id: "casos",
+      icon: <PiFolderUserDuotone />,
+      label: "Casos",
+      subItems: [
+        { id: "/casos", label: "Gestion Casos" },
+        { id: "/logscasos", label: "Actividad Casos" },
+      ],
+    },
+    { id: "/sabana", icon: <LuBookHeadphones />, label: "Sabanas" },
+  ],
+  5: [
+    { id: "/home_analista", icon: <FiHome />, label: "Inicio" },
+    {
+      id: "casos",
+      icon: <PiFolderUserDuotone />,
+      label: "Casos",
+      subItems: [
+        { id: "/casos", label: "Gestion Casos" },
+        { id: "/sabana", icon: <LuBookHeadphones />, label: "Sabanas" },
+      ],
+    },
+  ],
+};
